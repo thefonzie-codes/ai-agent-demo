16da997b3711ce1220a56bc078b372d6c1a33903
alfonsomanuel <alfonsomanuel@google.com>
2025-10-24 21:41:49 +0000

    database call working, and removed extra print call

--- main.py
+++ main.py
@@ -7,22 +7,34 @@ import os
 import sys
 import datetime
 
-from db import generate_db, delete_db
+from db import generate_db, delete_db, search_db
 
+search_db_declaration = {
+    "name": "search_db",
+    "description": "Searches the sqlite 3 database and returns a tuple of the values requested",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "sqlite3_query": {
+                "type": "string",
+                "description": """
+                An SQLITE3 query to search the database. This is the current schema:
+                        CREATE TABLE IF NOT EXISTS customers (
+                            id INTEGER PRIMARY KEY,
+                            first_name TEXT NOT NULL,
+                            last_name TEXT NOT NULL,
+                            email TEXT NOT NULL,
+                            phone TEXT NOT NULL,
+                            issue TEXT NOT NULL
+                        )
+                        """,
+            },
+        },
+        "required": ["sqlite3_query"],
+    },
+}
 
-def call_gemini_api(query):
-    """
-    We do NOT want to place the API key in the code.  Leaving it in the code allows others to view the key.
-    And if you're paying for that API key, and the cost increases based on usage - well, that won't be fun for you.
-    Instead, it's placed in the environment, or a dotfile.  Dotfiles are hidden by default.
-    We use a '.env' file to store configuration settings, environment variables etc.
-    Basically, this is sensitive information, securely.
-    """
-    try:
-        load_dotenv() #This loads the .env file
-    except Exception as e:
-        print(e)
-    
+def call_gemini_api(user_query):    
     """
     Try/Except:
     This is what we call 'error handling' - it is a way to deal with errors without crashing the program.
@@ -33,11 +45,42 @@ def call_gemini_api(query):
     # This calls the Gemini API
     try:
         client = genai.Client() #The API key located in the env file is found and automatically used
+        tools = types.Tool(function_declarations=[search_db_declaration])
+        config = types.GenerateContentConfig(tools=[tools])
+        contents = [
+            types.Content(
+                role="user", parts=[types.Part(text=user_query)]
+            )
+        ]
         response = client.models.generate_content(
             model="gemini-2.5-flash",
-            contents=query
+            contents=contents,
+            config=config
         )
-        return response.text
+        # print(response.candidates[0].content.parts[0].function_call)
+        if response.candidates[0].content.parts[0].function_call:
+          tool_call = response.candidates[0].content.parts[0].function_call
+          if tool_call.name == "search_db":
+            result = search_db(tool_call.args['sqlite3_query'])
+            print(f"TOOL CALL: {tool_call}")
+
+          function_response_part = types.Part.from_function_response(
+            name=tool_call.name,
+            response={"result": result},
+          )
+
+          contents.append(response.candidates[0].content) # Append the content from the model's response.
+          contents.append(types.Content(role="user", parts=[function_response_part])) # Append the function response
+
+          client = genai.Client()
+          final_response = client.models.generate_content(
+              model="gemini-2.5-flash",
+              config=config,
+              contents=contents,
+          )
+          return(final_response.candidates[0].content.parts[0].text)
+        # return(final_response.text)
+        return response.candidates[0].content.parts[0].text
     except Exception as e:
         print(e)
 
@@ -50,19 +93,32 @@ def read_db():
   cursor.execute()
 
 def main():
+  """
+  We do NOT want to place the API key in the code.  Leaving it in the code allows others to view the key.
+  And if you're paying for that API key, and the cost increases based on usage - well, that won't be fun for you.
+  Instead, it's placed in the environment, or a dotfile.  Dotfiles are hidden by default.
+  We use a '.env' file to store configuration settings, environment variables etc.
+  Basically, this is sensitive information, securely.
+  """
+  try:
+      load_dotenv() #This loads the .env file
+  except Exception as e:
+      print(e)
+      
   print("Generating Mock Customer Data...")
   generate_db()
   chats = []
   print("Hi! How can I help today?")
-  query = ""
-  while query != "exit":
-    query = input()
-    chats.append(f"🧑 User: {query}\n")
+  user_query = ""
+  while user_query != "exit":
+    user_query = input()
+    chats.append(f"🧑 User: {user_query}\n")
     context = "".join(chats)
-    context += f"🧑 User: {query}\n"
+    context += f"🧑 User: {user_query}\n"
     response = call_gemini_api(context)
     chats.append(f"🤖 Agent: {response}\n")
     print(response)
+
   with open(f"./chat_logs/{datetime.datetime.now()}", "a") as f:
     f.write("".join(chats))
   print("Deleting mock DB")
